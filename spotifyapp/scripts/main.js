b9a7ae3f589fbad971bfd1bb213bdd281f417b76
tistatos <tistatos@gmail.com>
2013-11-05 23:06:03 +0100

    Testing with getting current track playing

--- spotifyapp/scripts/main.js
+++ spotifyapp/scripts/main.js
@@ -1,9 +1,18 @@
 require([
-  '$api/models'
+  '$api/models',
   'scripts/button'
 ], function(models, button) {
   'use strict';
 
   button.doPlayButtonForTrack();
-
+  models.player.addEventListener('change', function()
+  {
+  	console.log(models.player);
+  	console.log(models.player.track);
+  	var track = document.getElementById('track');
+  	if(models.player.track != null)
+  	{
+  		track.innerHTML = models.player.track.name;
+  	}
+  });
 });
