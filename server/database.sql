5a1232ee2fad0c054cc2177412bb57e498113b2a
Klas Eskilson <klas.eskilson@gmail.com>
2013-11-12 23:38:40 +0100

    Addded database layout, draft 1 I guess

--- server/database.sql
+++ server/database.sql
@@ -5,6 +5,9 @@ CREATE DATABASE IF NOT EXISTS `hathor`;
 
 USE `hathor`;
 
+DROP TABLE IF EXISTS `quevote`;
+DROP TABLE IF EXISTS `quesong`;
+DROP TABLE IF EXISTS `parties`;
 DROP TABLE IF EXISTS `users`;
 
 CREATE TABLE IF NOT EXISTS `users` (
@@ -14,3 +17,43 @@ CREATE TABLE IF NOT EXISTS `users` (
 	`password` CHAR(130) NOT NULL,
 	PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE IF NOT EXISTS `parties` (
+	`partyid` INT(6) NOT NULL AUTO_INCREMENT,
+	`uid` INT(6) NOT NULL,
+	`name` CHAR(60) NOT NULL,
+	`hash` CHAR(5) NOT NULL,
+	`time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`locale` CHAR(2) NOT NULL,
+	PRIMARY KEY (`partyid`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+ALTER TABLE `parties`
+	ADD CONSTRAINT `parties_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
+
+CREATE TABLE IF NOT EXISTS `quesong` (
+	`songid` INT(6) NOT NULL AUTO_INCREMENT,
+	`uid` INT(6) NOT NULL,
+	`partyid` INT(6) NOT NULL,
+	`uri` CHAR(60) NOT NULL,
+	`played` BOOL DEFAULT 0,
+	PRIMARY KEY (`songid`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+ALTER TABLE `quesong`
+	ADD CONSTRAINT `quesong_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
+ALTER TABLE `quesong`
+	ADD CONSTRAINT `quesong_ibfk_2` FOREIGN KEY (`partyid`) REFERENCES `parties` (`partyid`);
+
+CREATE TABLE IF NOT EXISTS `quevote` (
+	`voteid` INT(6) NOT NULL AUTO_INCREMENT,
+	`songid` INT(6) NOT NULL,
+	`uid` INT(6) NOT NULL,
+	`time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`voteid`)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+ALTER TABLE `quevote`
+	ADD CONSTRAINT `quevote_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`);
+ALTER TABLE `quevote`
+	ADD CONSTRAINT `quevote_ibfk_2` FOREIGN KEY (`songid`) REFERENCES `quesong` (`songid`);
