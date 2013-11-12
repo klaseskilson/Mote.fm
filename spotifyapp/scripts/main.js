c81a627cc45ca192ff688977e0046e9561ac1a12
tistatos <tistatos@gmail.com>
2013-11-12 23:52:57 +0100

    forgot some files

--- spotifyapp/scripts/main.js
+++ spotifyapp/scripts/main.js
@@ -1,15 +1,13 @@
 require([
   '$api/models',
   'scripts/cover',
-  'scripts/track'
+  'scripts/track',
 ], function(models, cover, track) {
   'use strict';
-
   var tracks = new Array();
   tracks[0] = 'spotify:track:0Rynk2V7LyLgBUjTMxvbEJ';
   tracks[1] = 'spotify:track:4qw6yAygswKYFsO5GMybWu';
   tracks[2] = 'spotify:track:3vS2Jsk6g4Y8QMFsYZXr3z';
-
   var size  = tracks.length;
 
   for(var i = size-1; i >= 0; i--)
