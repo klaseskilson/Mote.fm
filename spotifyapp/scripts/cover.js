4a1f1efa25af7eb0b35f1eb9fa66b7309f4d61f1
danielronnkvist <daniel@ronnkvist.com>
2013-11-27 13:51:05 +0100

    New design according to plans. To be done: refresh when vote, cool animation when deleting

--- spotifyapp/scripts/cover.js
+++ spotifyapp/scripts/cover.js
@@ -5,14 +5,17 @@ require([
 	'use strict';
 
 	var insertImage = function(trackURI) {
-		models.Track.fromURI(trackURI).load('album').done(function(track) {
-			var album = models.Album.fromURI(track.album);
-			var image = Image.forAlbum(album, {player:false});
-			var id = 'cover';
-			document.getElementById(trackURI.substr(15,36)).getElementsByClassName(id)[0].appendChild(image.node);
-			image.setSize(100,100);
+		var album, link;
+		link = models.Track.fromURI(trackURI).load('album').done(function(track) {
+			models.Track.fromURI(trackURI).load('album').done(function(track) {
+                        var album = models.Album.fromURI(track.album);
+                        var image = Image.forAlbum(album, {player:false});
+                        var id = 'cover';
+                        document.getElementById(trackURI.substr(15,36)).getElementsByClassName(id)[0].appendChild(image.node);
+                        image.setSize(150,150);
+            });	
 		});
-	}
+	};
 
 	exports.insertImage = insertImage;
 });
