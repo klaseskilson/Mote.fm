fbaca34b656cc26b24bb1bfbc7d0e594a5f2118e
danielronnkvist <daniel@ronnkvist.com>
2013-12-04 23:29:52 +0100

    Nice transitions between the different things you can do on login-screen

--- spotifyapp/scripts/login.js
+++ spotifyapp/scripts/login.js
@@ -2,34 +2,60 @@ require([
   '$api/models',
   'scripts/jquery.min'
 ], function(models, jquery) {
-  $('#signUp').click(function() {
-  	if($('#name').css('display') == 'none')
-  	{
-	  	$('#email').animate({
-	      marginTop: '+=45px'
-	    }, 300, 'linear', function() {
-	    	$('#email').css('margin-top', '0px');
-			$('#name').css('display', 'inline');
-			$('#name').animate({
-				opacity: '1'
-			}, 500);
-	    });	
-  	}
-    
-  });
+	var situation = "#signIn";
 
-  $('#submit').click(function() {
-  	if($('#name').css('display') == 'none')
-  	{
-  		// Only login
-  		document.getElementById('xyz').innerHTML = "LOGIN";
-  		window.location.href = "party.html";
-  	}
-  	else
-  	{
-  		// Sign up
-  		document.getElementById('xyz').innerHTML = "SIGN UP";
-  	}
-  });
+	var switchButtons = function(id) {
+		console.log(id);
+		console.log(situation);
+		if($(id).hasClass('loginLeft'))
+		{
+			$(id).removeClass('loginLeft');
+			$(situation).addClass('loginLeft');
+		}
+		if($(id).hasClass('loginRight'))
+		{
+			$(id).removeClass('loginRight');
+			$(situation).addClass('loginRight');
+		}
+	}
+
+	$('#signUp').click(function() {
+		$('#password').show({duration: 400, queue: false});
+		$('#name').show({duration: 400, queue: false});
+		$('#name').attr('required', '');
+		switchButtons('#signUp');
+		situation = "#signUp";
+		$('#name').focus();
+	});
+
+	$('#forgotPwd').click(function() {
+		$('#password').hide({duration: 400, queue: false});
+		$('#name').hide({duration: 400, queue: false});
+		switchButtons('#forgotPwd');
+		situation = "#forgotPwd";
+		$('#email').focus();
+	});
+
+	$('#signIn').click(function() {
+		$('#password').show({duration: 400, queue: false});
+		$('#name').hide({duration: 400, queue: false});
+		switchButtons('#signIn');
+		situation = "#signIn";
+		$('#email').focus();
+	});
+
+	$('#submit').click(function() {
+		if($('#name').css('display') == 'none')
+		{
+			// Only login
+			document.getElementById('xyz').innerHTML = "LOGIN";
+			window.location.href = "party.html";
+		}
+		else
+		{
+			// Sign up
+			document.getElementById('xyz').innerHTML = "SIGN UP";
+		}
+	});
 
 });
